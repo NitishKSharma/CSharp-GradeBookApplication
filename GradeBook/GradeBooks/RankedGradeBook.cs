7d80a27153cd4bce01607c3a63b1ad6c841731b7
Nitish <sharma.nitish@live.com>
2019-05-25 02:28:46 -0400

    mod4

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -12,10 +12,10 @@ namespace GradeBook.GradeBooks
 
     public override char GetLetterGrade(double averageGrade)
     {
-      if(Students.Count < 5)
+      if (Students.Count < 5)
         throw new InvalidOperationException("Minimum number of students to create a Ranked Grade Book is 5");
 
-      switch(averageGrade)
+      switch (averageGrade)
       {
         case var grade when grade >= 80:
           return 'A';
@@ -23,15 +23,37 @@ namespace GradeBook.GradeBooks
         case var grade when grade >= 60 && grade < 80:
           return 'B';
 
-        case var grade when grade >=40 && grade < 60:
+        case var grade when grade >= 40 && grade < 60:
           return 'C';
 
-        case var grade when grade >=20 && grade < 40:
+        case var grade when grade >= 20 && grade < 40:
           return 'D';
 
         default:
           return 'F';
       }
+    }
+
+    public override void CalculateStatistics()
+    {
+      if (Students.Count < 5)
+      {
+        Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+        return;
+      }
+
+      base.CalculateStatistics();
+    }
+
+    public override void CalculateStudentStatistics(string name)
+    {
+      if(Students.Count > 5)
+      {
+        Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+        return;
+      }
+
+      base.CalculateStudentStatistics(name);
     }
   }
 }
