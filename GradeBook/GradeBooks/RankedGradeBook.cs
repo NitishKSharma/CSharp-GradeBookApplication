4b6555f0f9edb307eb82da2d6aea00905f9088f3
Nitish <sharma.nitish@live.com>
2019-05-25 02:23:36 -0400

    mod2

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,3 +1,4 @@
+using System;
 using GradeBook.Enums;
 
 namespace GradeBook.GradeBooks
@@ -7,6 +8,30 @@ namespace GradeBook.GradeBooks
     public RankedGradeBook(string name) : base(name)
     {
       Type = GradeBookType.Ranked;
+    }
+
+    public override char GetLetterGrade(double averageGrade)
+    {
+      if(Students.Count < 5)
+        throw new InvalidOperationException("Minimum number of students to create a Ranked Grade Book is 5");
+
+      switch(averageGrade)
+      {
+        case var grade when grade >= 80:
+          return 'A';
+
+        case var grade when grade >= 60 && grade < 80:
+          return 'B';
+
+        case var grade when grade >=40 && grade < 60:
+          return 'C';
+
+        case var grade when grade >=20 && grade < 40:
+          return 'D';
+
+        default:
+          return 'F';
+      }
     }
   }
 }
